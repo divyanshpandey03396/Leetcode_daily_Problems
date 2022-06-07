43143da911eda596e532ea5a00bfaa1b90009a5b
divyanshpandey03396 <divyanshpandey03396@gmail.com>
2022-06-07 13:09:07 +0530

    +1

---
+++ Merge_sorted_array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+      int i = m - 1,// first pointer
+        j = n - 1, // second  pointer
+        k = m + n - 1;// third pointer
+     while(i >= 0 and j >= 0) 
+     {
+         
+         if(nums1[i] < nums2[j]) 
+             nums1[k--] = nums2[j--];
+         
+          else 
+             nums1[k--] = nums1[i--];
+         
+     }
+     while(j >= 0) { // following part is the case when nums1 is empty and nums2 remains
+         nums1[k--] = nums2[j--];
+     }
+        
+    }
+};
