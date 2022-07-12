e9906a4ca362070ae88af35ec1d666e8f5df483e
divyanshpandey03396 <divyanshpandey03396@gmail.com>
2022-07-12 11:58:49 +0530

    +2

---
+++ right_view_of_bt.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void solve(TreeNode* root,int level,vector<int>&v){
+        if(!root) return;
+        if(level==v.size()) v.push_back(root->val);
+        solve(root->right,level+1,v);
+        solve(root->left,level+1,v);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>v;
+        solve(root,0,v);
+        return v;
+    }
+};
